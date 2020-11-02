607b02b5f0e55adbc27e81c4bf4fd987a3fb6430
Jason Carrick <jasoncarrick@workfront.com>
2020-11-02 10:36:01 -0700

    test: Add tests for getRandomNumber

--- src/util/getRandomNumber.js
+++ src/util/getRandomNumber.js
@@ -1,3 +1,7 @@
 export function getRandomNumber(min, max) {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new Error('min and max must be numbers')
+  }
+
   return Math.floor(Math.random() * (max - min)) + min
 }
---
+++ src/util/getRandomNumber.test.js
@@ -0,0 +1,20 @@
+import { getRandomNumber } from './getRandomNumber.js'
+
+describe('getRandomNumber', () => {
+  it('returns a number', () => {
+    expect(typeof getRandomNumber(0, 5)).toBe('number')
+  })
+
+  it('returns number between min and max', () => {
+    const min = 0
+    const max = 5
+    const result = getRandomNumber(min, max)
+
+    expect(result).toBeGreaterThanOrEqual(min)
+    expect(result).toBeLessThanOrEqual(max)
+  })
+
+  it('throws an error if a number is not passed for min or max', () => {
+    expect(() => getRandomNumber('hello', 'world')).toThrow()
+  })
+})
