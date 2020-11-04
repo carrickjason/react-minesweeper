8ce6039b8e6834dfa716898d29709c097117fc43
Tyler Hawkins <tylerhawkins@workfront.com>
2020-11-04 10:37:22 -0700

    refactor: extracts number input into own component

---
+++ src/FormNumberInput.js
@@ -0,0 +1,27 @@
+import React, { forwardRef } from 'react'
+
+export const FormNumberInput = forwardRef(
+  ({ label, id, value, min, max }, ref) => (
+    <div className="md:flex md:items-center mb-6">
+      <div className="md:w-1/3">
+        <label
+          className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+          htmlFor={id}
+        >
+          {label}
+        </label>
+      </div>
+      <div className="md:w-2/3">
+        <input
+          id={id}
+          type="number"
+          defaultValue={value}
+          min={min}
+          max={max}
+          ref={ref}
+          className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+        />
+      </div>
+    </div>
+  )
+)
--- src/NewGameControls.js
+++ src/NewGameControls.js
@@ -1,5 +1,6 @@
 import React, { useRef } from 'react'
 
+import { FormNumberInput } from './FormNumberInput'
 import { START_GAME } from './actions/constants'
 
 const NewGameControls = ({ width, height, numMines, dispatch }) => {
@@ -10,7 +11,7 @@ const NewGameControls = ({ width, height, numMines, dispatch }) => {
   return (
     <div>
       <form
-        className="w-1/4 m-auto"
+        className="w-1/2 m-auto"
         onSubmit={e => {
           dispatch({
             type: START_GAME,
@@ -23,71 +24,32 @@ const NewGameControls = ({ width, height, numMines, dispatch }) => {
           e.preventDefault()
         }}
       >
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="width"
-            >
-              Width:
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              id="width"
-              type="number"
-              defaultValue={width}
-              min="1"
-              max="20"
-              ref={widthEl}
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            />
-          </div>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="height"
-            >
-              Height:
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              id="height"
-              type="number"
-              defaultValue={height}
-              min="1"
-              max="20"
-              ref={heightEl}
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            />
-          </div>
-        </div>
-        <div class="md:flex md:items-center mb-6">
-          <div class="md:w-1/3">
-            <label
-              class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-              for="numMines"
-            >
-              Number of mines:
-            </label>
-          </div>
-          <div class="md:w-2/3">
-            <input
-              id="numMines"
-              type="number"
-              defaultValue={numMines}
-              min="1"
-              max="50"
-              ref={numMinesEl}
-              class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
-            />
-          </div>
-        </div>
+        <FormNumberInput
+          label="Number of Columns:"
+          id="width"
+          min={1}
+          max={20}
+          ref={widthEl}
+          value={width}
+        />
+        <FormNumberInput
+          label="Number of Rows:"
+          id="height"
+          min={1}
+          max={20}
+          ref={heightEl}
+          value={height}
+        />
+        <FormNumberInput
+          label="Number of Mines:"
+          id="numMines"
+          min={1}
+          max={50}
+          ref={numMinesEl}
+          value={numMines}
+        />
         <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
           Start
