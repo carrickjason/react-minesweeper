42ca2f78d9eda0693d7e3c542634a3b816cac87b
Clint Goodman <clintgoodman@workfront.com>
2020-11-05 10:30:16 -0700

    feat: adding GUID input

---
+++ src/FormInput.js
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react'
+
+export const FormInput = forwardRef(({ label, id, value, ...rest }, ref) => (
+  <div className="md:flex md:items-center mb-6">
+    <div className="md:w-1/3">
+      <label
+        className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+        htmlFor={id}
+      >
+        {label}
+      </label>
+    </div>
+    <div className="md:w-2/3">
+      <input
+        id={id}
+        defaultValue={value}
+        ref={ref}
+        className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+        {...rest}
+      />
+    </div>
+  </div>
+))
--- src/NewGameControls.js
+++ src/NewGameControls.js
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react'
 
 import { FormNumberInput } from './FormNumberInput'
+import { FormInput } from './FormInput'
 import { START_GAME } from './actions/constants'
 
 const NewGameControls = ({ width, height, numMines, dispatch }) => {
@@ -9,9 +10,9 @@ const NewGameControls = ({ width, height, numMines, dispatch }) => {
   const numMinesEl = useRef(null)
 
   return (
-    <div>
+    <div className="w-1/2 m-auto">
       <form
-        className="w-1/2 m-auto"
+        className="shadow-md rounded px-8 pt-6 pb-8 mb-8 bg-white"
         onSubmit={e => {
           dispatch({
             type: START_GAME,
@@ -52,7 +53,17 @@ const NewGameControls = ({ width, height, numMines, dispatch }) => {
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
-          Start
+          Create New Game
+        </button>
+      </form>
+      OR
+      <form className="shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-white mt-8">
+        <FormInput label="Enter a UUID:" id="uuid" />
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          type="submit"
+        >
+          Start Game
         </button>
       </form>
     </div>
