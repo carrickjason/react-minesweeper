7d78fec63a6f2a81ab7c9f04d03bdba87711de56
Jason Carrick <jasoncarrick@workfront.com>
2020-11-12 10:54:22 -0700

    refactor: better variable names and test

--- src/util/getAllPossibleCoordinates.js
+++ src/util/getAllPossibleCoordinates.js
@@ -3,9 +3,9 @@ const neighborCoordinates = [1, 0, -1]
 export function getAllPossibleCoordinates(x, y) {
   const positions = []
 
-  neighborCoordinates.forEach(xMod => {
-    neighborCoordinates.forEach(yMod => {
-      positions.push([x + xMod, y + yMod])
+  neighborCoordinates.forEach(xNeighborCoordinate => {
+    neighborCoordinates.forEach(yNeighborCoordinate => {
+      positions.push([x + xNeighborCoordinate, y + yNeighborCoordinate])
     })
   })
 
--- src/util/getAllPossibleCoordinates.test.js
+++ src/util/getAllPossibleCoordinates.test.js
@@ -1,3 +1,18 @@
+import { getAllPossibleCoordinates } from './getAllPossibleCoordinates'
+
 describe('getAllPossibleCoordinates', () => {
-  it('passes', () => {})
+  it('returns list of neighboring coordinates', () => {
+    const neighbors = getAllPossibleCoordinates(3, 3)
+    expect(neighbors).toEqual([
+      [4, 4],
+      [4, 3],
+      [4, 2],
+      [3, 4],
+      [3, 3],
+      [3, 2],
+      [2, 4],
+      [2, 3],
+      [2, 2],
+    ])
+  })
 })
