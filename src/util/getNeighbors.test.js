345ba0a685662a979a0a335bad3a89e8dbe13065
Tyler Hawkins <tylerhawkins@workfront.com>
2020-11-13 09:34:54 -0700

    test: adds tests for coordinate methods

--- src/util/getNeighbors.test.js
+++ src/util/getNeighbors.test.js
@@ -1,3 +1,31 @@
+import { getNeighbors } from './getNeighbors'
+import { makeGameGrid } from './makeGameGrid'
+
 describe('getNeighbors', () => {
-  it('passes', () => {})
+  it('returns list of neighboring coordinates for a cell that has neighbors on all sides', () => {
+    const grid = makeGameGrid({ height: 4, width: 4, numMines: 3 })
+    const neighbors = getNeighbors(grid, 2, 2)
+    expect(neighbors).toEqual([
+      [3, 3],
+      [3, 2],
+      [3, 1],
+      [2, 3],
+      [2, 2],
+      [2, 1],
+      [1, 3],
+      [1, 2],
+      [1, 1],
+    ])
+  })
+
+  it('returns list of neighboring coordinates for a cell in the upper-left corner of the grid', () => {
+    const grid = makeGameGrid({ height: 4, width: 4, numMines: 3 })
+    const neighbors = getNeighbors(grid, 0, 0)
+    expect(neighbors).toEqual([
+      [1, 1],
+      [1, 0],
+      [0, 1],
+      [0, 0],
+    ])
+  })
 })
--- src/util/removeCoordinatesOutsideOfGrid.test.js
+++ src/util/removeCoordinatesOutsideOfGrid.test.js
@@ -1,3 +1,52 @@
+import { removeCoordinatesOutsideOfGrid } from './removeCoordinatesOutsideOfGrid'
+
 describe('removeCoordinatesOutsideOfGrid', () => {
-  it('passes', () => {})
+  it('returns list of valid neighboring coordinates for a cell that has neighbors on all sides', () => {
+    const allPossibleNeighbors = [
+      [4, 4],
+      [4, 3],
+      [4, 2],
+      [3, 4],
+      [3, 3],
+      [3, 2],
+      [2, 4],
+      [2, 3],
+      [2, 2],
+    ]
+
+    const validNeighbors = removeCoordinatesOutsideOfGrid(
+      allPossibleNeighbors,
+      5,
+      5
+    )
+    expect(validNeighbors).toEqual(allPossibleNeighbors)
+  })
+
+  it('returns list of valid neighboring coordinates for a cell in the upper-left corner of the grid', () => {
+    const allPossibleNeighbors = [
+      [1, 1],
+      [1, 0],
+      [1, -1],
+      [0, 1],
+      [0, 0],
+      [0, -1],
+      [-1, 1],
+      [-1, 0],
+      [-1, -1],
+    ]
+
+    const expectedValidNeighbors = [
+      [1, 1],
+      [1, 0],
+      [0, 1],
+      [0, 0],
+    ]
+
+    const validNeighbors = removeCoordinatesOutsideOfGrid(
+      allPossibleNeighbors,
+      3,
+      3
+    )
+    expect(validNeighbors).toEqual(expectedValidNeighbors)
+  })
 })
