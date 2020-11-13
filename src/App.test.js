dd396282e912d043b9a400302658f27edb5a427a
Tyler Hawkins <tylerhawkins@workfront.com>
2020-11-13 11:37:30 -0700

    test: adds tests for App.js

---
+++ src/App.test.js
@@ -0,0 +1,29 @@
+import App from './App'
+import { render, screen, fireEvent } from '@testing-library/react'
+
+describe('App', () => {
+  it('renders the new game controls when a game has not been started yet', () => {
+    render(<App />)
+
+    expect(screen.getByText('React Minesweeper')).toBeInTheDocument()
+
+    expect(screen.getByText('Number of Columns:')).toBeInTheDocument()
+    expect(screen.getByText('Number of Rows:')).toBeInTheDocument()
+    expect(screen.getByText('Number of Mines:')).toBeInTheDocument()
+    expect(screen.getByText('Create New Game')).toBeInTheDocument()
+
+    expect(screen.getByText('Enter a UUID:')).toBeInTheDocument()
+    expect(screen.getByText('Start Game')).toBeInTheDocument()
+  })
+
+  it('renders the game board when a game is in progress', () => {
+    render(<App />)
+    fireEvent.click(screen.getByText('Create New Game'))
+
+    expect(screen.getByText('React Minesweeper')).toBeInTheDocument()
+
+    expect(screen.getByText('Flags Left: 1')).toBeInTheDocument()
+    expect(screen.getByText('Game Time:')).toBeInTheDocument()
+    expect(screen.getByText('Quit')).toBeInTheDocument()
+  })
+})
