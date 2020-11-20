bc5ccdd39c208270df7a5db9537e72c7e0a50672
Tyler Hawkins <tylerhawkins@workfront.com>
2020-11-20 10:48:16 -0700

    test: adds tests for cell aria labels

---
+++ src/Cell.test.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+
+import { Cell } from './Cell'
+
+const noop = () => {}
+
+describe('Cell', () => {
+  it('renders an unswept cell', () => {
+    render(
+      <Cell
+        isSwept={false}
+        isFlagged={false}
+        clickHandler={noop}
+        hasMine={false}
+      />
+    )
+    expect(screen.getByLabelText('Unswept Cell')).toBeInTheDocument()
+  })
+
+  it('renders a flagged and unswept cell', () => {
+    render(
+      <Cell isSwept={false} isFlagged clickHandler={noop} hasMine={false} />
+    )
+    expect(screen.getByAltText('Flagged Cell')).toBeInTheDocument()
+  })
+
+  it('renders a swept cell that contains no text', () => {
+    render(
+      <Cell isSwept isFlagged={false} clickHandler={noop} hasMine={false} />
+    )
+    expect(screen.getByLabelText('Swept Cell')).toBeInTheDocument()
+  })
+})
