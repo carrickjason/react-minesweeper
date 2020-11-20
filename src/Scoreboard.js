11c4572701db016cc8193340e7a0c636be503a9f
Jason Carrick <jasoncarrick@workfront.com>
2020-11-20 09:49:33 -0700

    fix: make sure scorebooard text doesn't wrap

--- src/Scoreboard.js
+++ src/Scoreboard.js
@@ -4,11 +4,11 @@ import Timer from './Timer'
 const Scoreboard = ({ numRemainingFlags, delay }) => {
   return (
     <div className="text-xl text-center text-lightBlue flex lg:w-2/3 w-full m-auto mb-8 lg:justify-center items-center flex-col lg:flex-row">
-      <p className="flex items-center justify-center lg:w-1/3 m-4 min-w-max bg-darkBlue rounded p-4">
+      <p className="flex items-center justify-center w-2/3 sm:w-1/2 lg:w-1/3 m-4 min-w-max bg-darkBlue rounded p-4 whitespace-no-wrap">
         <span className="text-2xl mr-8">{numRemainingFlags}</span>{' '}
         {numRemainingFlags === 1 ? 'Flag' : 'Flags'} Left
       </p>
-      <p className="flex items-center justify-center lg:w-1/3 m-4 min-w-max bg-darkBlue rounded p-4">
+      <p className="flex items-center justify-center w-2/3 sm:w-1/2 lg:w-1/3 m-4 min-w-max bg-darkBlue rounded p-4">
         Game Time <Timer delay={delay} />
       </p>
     </div>
